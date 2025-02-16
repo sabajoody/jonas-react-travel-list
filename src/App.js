86b6e60d33c7587b0a6968b7fae3a36a6cfd4f5e
Joodi <sabajokar81@gmail.com>
2025-02-16 17:42:22 +0330

    edit status function completed (S7, EP7)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "charger", quantity: 3, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "charger", quantity: 3, packed: true },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handelAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handelDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handelChangeStatus(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handelAddItems} />
+      <PackingList
+        items={items}
+        onDelete={handelDeleteItems}
+        onStatusChange={handelChangeStatus}
+      />
       <Stats />
     </div>
   );
@@ -21,7 +41,7 @@ function Logo() {
   return <h1> Far Away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState(""); //controlled elements
   const [quantity, setQuantity] = useState(1);
 
@@ -32,6 +52,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -44,7 +66,6 @@ function Form() {
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
       >
-        {/* value comes from<option> */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -62,25 +83,37 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDelete, onStatusChange }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDelete={onDelete}
+            onStatusChange={onStatusChange}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDelete, onStatusChange }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        // value={item.packed}
+        onClick={() => onStatusChange(item.id)}
+      ></input>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button style={{ color: "red" }}>X</button>
+      <button style={{ color: "red" }} onClick={() => onDelete(item.id)}>
+        X
+      </button>
     </li>
   );
 }
